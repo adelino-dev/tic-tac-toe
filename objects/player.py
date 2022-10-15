5c95409587dc99bc24ae3851aaa5dd305c342901
adelino-dev <adelino.developer@gmail.com>
2022-10-15 02:52:44 -0300

    feat: add Player

---
+++ objects/player.py
@@ -0,0 +1,30 @@
+class Player(object):
+    """
+    Create a player object for the  to play Tic-Toc-Toe.
+    """
+    def __init__(self, symbol):
+        """
+        symbol --> a string (X or O)
+        """
+        self._symbol = symbol
+    
+    def markSquare(grid, position):
+        """
+        Paramenters:
+            grid --> a Grid object.
+            position --> a letter (A-I) representing the position
+            
+            ╔═════════════╗
+            ║  A │ B │ C  ║
+            ║────┼───┼────║
+            ║  D │ E │ F  ║
+            ║────┼───┼────║
+            ║  G │ H │ I  ║
+            ╚═════════════╝
+
+        ---------------------------------
+
+        Set the symbol in the indicated position.
+
+        """
+        grid.markSquare(position, self._symbol)
