6080fe4fb8d8292e0e811f8d13653fbdc1622cbd
adelino-dev <adelino.developer@gmail.com>
2022-10-17 18:18:42 -0300

    doc: fix the quotation marks in the function getSymbol in Player object

--- objects/player.py
+++ objects/player.py
@@ -30,5 +30,12 @@ class Player(object):
         grid.markSquare(position, self._symbol)
     
     def getSymbol(self):
-        "Return the symbol (X or O)."
+        """
+        Return the symbol (X or O).
+        """
+        coord ={
+            'A':(0,0), 'B':(0,1), 'C':(0,2),
+            'D':(1,0), 'E':(1,1), 'F':(1,2),
+            'G':(2,0), 'H':(2,1), 'I':(2,2),
+        }
         return self._symbol
